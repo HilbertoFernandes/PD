74af262adf23a7ea10c825dec33ad8896bcb492f
Hilberto Fernandes <hilbertofernandes@gmail.com>
2019-02-23 14:05:08 -0300

    PD

--- 10 - Miniprojeto-3/FilmeCliente/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/htdocs/FilmeCliente/deletar.php
+++
@@ -1,6 +0,0 @@
-<?php
-$titulo = $_GET['id'];
-$ws = new SoapClient('http://localhost:8080/FilmeService/FilmeService?WSDL');
-$ws->__soapCall('excluirFilme', array('parameters' => array('id' => $titulo)));
-header('Location: http://localhost:8090/FilmeCliente/index.php');
-?>
